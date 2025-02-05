31dfd79528a6e7d04e63ff82ff9ec7d4a51125a9
Aleksandra <aleksandrazatirka21@gmail.com>
2025-02-05 18:40:25 -0500

    books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,37 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   let bookbyauthor = books.filter((book) => {
+        return book.author === author;
+    });
+    if (bookbyauthor.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookbytitle = books.filter((book) => {
+        return book.title === title;
+    });
+    if (bookbytitle.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 });
 
 //  Get book review
